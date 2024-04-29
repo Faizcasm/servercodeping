b6ebab9fc1fd450593b5c1cbcd6c2ca98fff2f8c
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-04-29 07:20:29 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
+import router from './router.js'
 import dotenv from 'dotenv'
-import {createServer} from 'http'
+
 const app =express()
 const server = createServer(app)
 const port =500;
@@ -34,17 +35,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use('/', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type , Accept');
-      res.header('Content-type','text/plain, application/octet-stream , application/json ,image/apng,image/avif')
-    next();
-  });
-  const options={
       origin:'http://localhost:5173',
       credentials:true,
        methods: ["GET", "POST","PUT","DELETE"],
   }
 app.use(cors(options))
-import router from './router.js'
+
 app.use('/',router)

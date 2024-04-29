dc213568e699c428cf8022cccf2c4d9386390eb2
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-04-29 04:49:45 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,20 +38,16 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use('/', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  next();
+});
 const options={
     origin:'http://localhost:5173',
     credentials:true,
      methods: ["GET", "POST","PUT","DELETE"],
-    allowedHeaders: ["my-custom-header"],
-Content-Type:"application/json"
-
 }
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 app.use(cors(options))
 import router from './router.js'
 app.use('/',router)

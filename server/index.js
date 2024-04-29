98b0a9aeda63cf9c9ec38c1fc7b38f792a8500dd
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-04-29 01:59:20 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
 const options={
-    origin:['*'],
+    origin:'https://codeping.vercel.app',
     credentials:true,
      methods: ["GET", "POST","PUT","DELETE"],
     allowedHeaders: ["my-custom-header"],

64f42f4890c3ffc02c529d7154e853d77596b0a0
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-04-29 04:38:00 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,12 +33,7 @@ database()
 app.get('/',async(req,res)=>{
      res.send("Codeping server is up ")
     })
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
@@ -47,8 +42,16 @@ const options={
     origin:'http://localhost:5173',
     credentials:true,
      methods: ["GET", "POST","PUT","DELETE"],
-    allowedHeaders: ["my-custom-header"]
+    allowedHeaders: ["my-custom-header"],
+Content-Type:"application/json"
+
 }
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(cors(options))
 import router from './router.js'
 app.use('/',router)

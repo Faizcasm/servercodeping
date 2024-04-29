b27aa24410767cfaf00564b00daa80155a38c1c4
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-04-29 02:03:31 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const options={
     origin:'https://codeping.vercel.app',
     credentials:true,
      methods: ["GET", "POST","PUT","DELETE"],
-    allowedHeaders: ["my-custom-header"],
+    allowedHeaders: ["*"],
 }
 app.use(cors(options))
 import router from './router.js'

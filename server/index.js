e4b8403f2cbe3e2cacd7002a2ef10921b47c4ca5
Faizan Hameed <138277686+Faizcasm@users.noreply.github.com>
2024-04-29 02:20:52 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,6 +33,12 @@ database()
 app.get('/',async(req,res)=>{
      res.send("i am also working")
     })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
